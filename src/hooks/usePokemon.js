fa9fcd6a4f23fe92bf6fee6da016efd19dd81c2a
KeanePutraSetiawan <keane@glad-cube.com>
2024-06-06 17:22:03 +0900

    add api to fetch pokemon names

---
+++ src/hooks/usePokemon.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const usePokemon = (pokemonName) => {
+  const {
+    isLoading: pokemonLoading,
+    isError: pokemonError,
+    data: pokemonData,
+  } = useQuery({
+    queryKey: ["pokemonName", pokemonName],
+    queryFn: async () => {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      return res.data;
+    },
+  });
+
+  return [pokemonLoading, pokemonError, pokemonData];
+};
+export default usePokemon;
